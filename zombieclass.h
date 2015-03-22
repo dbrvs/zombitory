5a5f642577802f4e48edc5ddf31477e63cd6e9bf
howard <howardfk@gmail.com>
2015-03-22 13:28:46 -0400

    changed zombie class to population class, and added varibles and functions, not compilable

---
+++ zombieclass.h
@@ -0,0 +1,15 @@
+
+//this is kinda all hog wash but i just wanted to test git out
+
+class Population{
+private:float pop_infec, pop_norm
+
+
+void infection(pop_norm){
+   pop_norm - pop_infec(pop_norm)
+   
+}
+
+void pop_norm
+
+}
